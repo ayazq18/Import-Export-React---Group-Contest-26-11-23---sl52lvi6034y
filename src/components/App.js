c1bb39c6cd0f22347bc028de77deb83b533b2b02
ayazq18@gmail.com <131868270+ayazq18@users.noreply.github.com>
2023-11-26 18:23:52 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import Heading from "./Heading"
+import SubHeading from "./SubHeading";
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <SubHeading />
+      <InputQuery />
+      <SubmitButton />
     </div>
   )
 }
 
-
+export default App
